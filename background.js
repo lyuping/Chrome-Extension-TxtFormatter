4391065c995217eeec23c2498d94331b4db15ad2
lyuping <flytolonely@gmail.com>
2015-04-18 23:29:05 +0800

    change to non-jQuery version

--- background.js
+++ background.js
@@ -1,35 +1,24 @@
 /*
  * Copyright Yuping Lin 2015
+ * 
+ * todo: 
+ * hook input focus on button
+ * pin/unpin at top
+ * bookmark(local cookies problem)
+ * line-height allow float, need regex
+ * chapter regex rule like (1) ¡]¤@¡^
+ * title hight light
+ * hight light background
+ * (no need maybe) background/font color picker
  */
 
 "use strict";
 
+/* BACKGROUND */
+
 function LoadBasic(tab) {
     chrome.tabs.insertCSS(null, { file: 'style.css', "allFrames": true });
-    chrome.tabs.executeScript(null, { file: "jquery-2.1.3.min.js" }, function () {
-        chrome.tabs.executeScript(null, { file: "txtReader.js" });
-    });
-}
-
-function SetCookies() {
-    chrome.cookies.set({
-        "name": "txtReader",
-        "url": "http://127.0.0.1/",
-        "value": "Dummy Data"
-    }, function (cookie) {
-        console.log(JSON.stringify(cookie));
-        console.log(chrome.extension.lastError);
-        console.log(chrome.runtime.lastError);
-        debugger;
-    });
+    chrome.tabs.executeScript(null, { file: "txtReader.js" });
 }
 
 chrome.browserAction.onClicked.addListener(LoadBasic);
-
-chrome.extension.isAllowedIncognitoAccess(function (isAllowedAccess) {
-    if (isAllowedAccess) return; // Great, we've got access
-
-    chrome.tabs.create({
-        url: 'chrome://extensions/?id=' + chrome.runtime.id
-    });
-});
--- txtReader.js
+++ txtReader.js
@@ -2,121 +2,143 @@
  * Copyright Yuping Lin 2015
  */
 
-"use strict";
+'use strict';
 
-var themeDropdown = {
-    target: $("<select id='dpTheme'></select>"),
-    data: { "default": "-- 選擇布景 --", "black": "黑色布景", "darkGray": "深灰布景", "gray": "灰色布景" },
-    getDropdownfunction: function () {
-        for (var val in this.data) {
-            $("<option />", { value: val, text: this.data[val] }).appendTo(this.target);
+
+var regexPtrn = {
+    float: /[-+]?([0-9]*.[0-9]+|[0-9]+)/g,
+    prologue: /楔子.*/g,
+    chapter: /(第(.+)章.*)/g,
+    chapter2: /(第(.?)百?(.?)十?(.)章)?(第$.?章$)?/g,
+    br: /(?:\\[rn]|[\r\n]+)+/g
+};
+
+var dpData = {
+    theme: [{ key: 'default', text: '-- 預設布景 --' }, { key: 'black', text: '黑色布景' }, { key: 'darkGray', text: '深灰布景' }, { key: 'gray', text: '灰色布景' }],
+    fontFamily: [{ key: '', text: '-- 預設字體 --' }, { key: 'SimSun', text: '新宋體' }, { key: 'Microsoft_YaHei', text: '微軟雅黑' }, { key: 'Microsoft_JhengHei', text: '微軟正黑' }],
+};
+
+var lib = {
+    getDom: function (id) {
+        return document.getElementById(id);
+    },
+    createDom: function (type) {
+        return document.createElement(type);
+    },
+    createDropdown: function (id, data, onChangeFn) {
+        var dp = this.createDom('select');
+        dp.id = id;
+
+        for (var i = 0; i < data.length; i++) {
+            var option = this.createDom('option');
+            option.value = data[i].key;
+            option.text = data[i].text;
+            dp.appendChild(option);
         }
-        return this.target;
-    },
-    bindEvent: function () {
-        this.target.on("change", function (evt) {
-            $("body").prop("class", this.selectedOptions[0].value);
-        });
-    },
-    init: function () {
-        this.getDropdownfunction();
-        this.bindEvent();
-        return this.target;
-    }
-}
 
-var fontDropdown = {
-    target: $("<select id='dpFontFamily'></select>"),
-    data: { "": "-- 預設字體 --", "SimSun": "新宋體", "Microsoft_YaHei": "微軟雅黑", "Microsoft_JhengHei": "微軟正黑" },
-    getDropdownfunction: function () {
-        for (var val in this.data) {
-            $("<option />", { value: val, text: this.data[val] }).appendTo(this.target);
+        onChangeFn && (dp.onchange = function (e) {
+            onChangeFn(e);
+        });
+        return dp;
+    },
+    createInput: function (id, inputAttrs, btnText, onClickEvt, onKeyDownEvt) {
+        var span = this.createDom('span');
+        var input = this.createDom('input');
+        var btn = this.createDom('button');
+        input.id = id;
+        input.type = 'text';
+        for (var i = 0, al = inputAttrs.length; i < al; i++) {
+            input.setAttribute(inputAttrs[i].name, inputAttrs[i].value);
         }
-        return this.target;
-    },
-    bindEvent: function () {
-        this.target.on("change", function (evt) {
-            var font = this.selectedOptions[0].value;
-            font = font.replace("_", " ");
-            $("body").css("font-family", font);
-        });
-    },
-    init: function () {
-        this.getDropdownfunction();
-        this.bindEvent();
-        return this.target;
-    }
-}
 
-var fontSizeInput = {
-    id: "FontSize",
-    container: $("<span id='"+ this.id +"'></span>"),
-    wrapUI: function () {
-        //this.container.append("<label>更改字體大小</label>");
-        this.container.append("<input type='text' id='tb" + this.id + "' placeholder=' px' maxlength='2' value='16' />");
-        this.container.append("<button id='btn" + this.id + "'>更改字體大小</button>");
-    },
-    bindEvent: function () {
-        this.container.on("click", "#btn" + this.id + "", function (evt) {
-            //todo: regex
-            var size = $("#tbFontSize").val();
-            if (size && size > 0) {
-                $("body").css("font-size", size + 'px');
+        btn.id = id + 'Button';
+        btn.innerHTML = btnText;
+
+        onClickEvt && (btn.onclick = function (e) {
+            onClickEvt(e);
+        });
+
+        onKeyDownEvt && (input.onkeyup = function (e) {
+            onKeyDownEvt(e);
+        });
+
+        input.onkeydown = function (e) {
+            if (e.keyCode == 13) {
+                e.currentTarget.nextElementSibling.focus();
+                e.currentTarget.nextElementSibling.click();
             }
-        });
-    },
-    init: function () {
-        this.wrapUI();
-        this.bindEvent();
-        return this.container;
-    }
-}
+        };
 
-var lineHeightInput = {
-    id: "LineHeight",
-    container: $("<span id='" + this.id + "'></span>"),
-    wrapUI: function () {
-        this.container.append("<input id='tb" + this.id + "' type='text' placeholder=' 倍' maxlength='1' value='2' />");
-        this.container.append("<button id='btn" + this.id + "'>更改行高</button>");
-    },
-    bindEvent: function () {
-        this.container.on("click", "#btn" + this.id, function (evt) {
-            //todo: regex & float
-            var size = $("#tbLineHeight").val();
-            if (size && size > 0) {
-                $("body").css("line-height", size + "em");
+        span.appendChild(input);
+        span.appendChild(btn);
+        return span;
+    }
+};
+
+
+function initFunc() {
+    //init
+    var func = document.createElement('div');
+    func.id = 'func';
+    func.className = 'default';
+    document.body.insertBefore(func, lib.getDom('container'));
+
+
+    var queue = [];
+    queue.push(lib.createDropdown('dpTheme', dpData.theme, function (e) {
+        document.body.className = e.currentTarget.value;
+    }));
+    queue.push(lib.createDropdown('dpFontFamily', dpData.fontFamily, function (e) {
+        document.body.style.fontFamily = lib.getDom('dpFontFamily').value.replace('_', ' ');
+    }));
+
+    queue.push(lib.createInput('tbFontSize'
+        , [{ name: 'placeholder', value: ' px' }, { name: 'maxlength', value: '2' }, { name: 'value', value: '16' }]
+        , '更改字體大小'
+        , function (e) {
+            document.body.style.fontSize = lib.getDom('tbFontSize').value + 'px';
+        }
+    ));
+
+    queue.push(lib.createInput('tbLineHeight'
+        , [{ name: 'placeholder', value: ' 倍' }, { name: 'maxlength', value: '3' }, { name: 'value', value: '2' }]
+        , '更改行高'
+        , function (e) {
+            document.body.style.lineHeight = lib.getDom('tbLineHeight').value + 'em';
+        }, function (e) {
+            var value = e.currentTarget.value;
+            if (!value.match(/[0-9]+|\./)) {
+                e.currentTarget.value = value.replace(value.slice(-1), "");
             }
-        });
-    },
-    init: function () {
-        this.wrapUI();
-        this.bindEvent();
-        return this.container;
+        }
+    ));
+
+    for (var i = 0, q = queue.length; i < q; i++) {
+        func.appendChild(queue[i]);
     }
 }
 
-function initFunctionSet() {
-    var func = $("<div id='func'></div>");
-    func.append(themeDropdown.init());
-    func.append(fontDropdown.init());
-    func.append(fontSizeInput.init());
-    func.append(lineHeightInput.init());
-
-    $("body").prop("class", "default").prepend(func);
+function txtFmt(text) {
+    var rtn = '';
+    rtn = text.replace(regexPtrn.prologue, '<h1>$1</h1>');
+    rtn = text.replace(regexPtrn.chapter, '<h1>$1</h1>');
+    rtn = text.replace(regexPtrn.br, '</p><p>');
+    return rtn;
 }
 
-function txtFormatter() {
-    var txt = $("pre").html();
-    txt = txt.replace(/(第(.+)章.*)/g, "<h1>$1</h1>");
-    txt = txt.replace(/楔子.*/, "<h1>楔子</h1>");
-    txt = txt.replace(/(?:\\[rn]|[\r\n]+)+/g, "</p><p>");
-    $("pre").replaceWith("<div id='container'><p>" + txt + "</p></div>");
+function wrapAndFmt() {
+    var div, pre;
+    pre = document.getElementsByTagName('pre')[0];
+    div = lib.createDom('div');
+    div.id = 'container';
+    div.innerHTML = txtFmt(pre.innerHTML);
+    document.body.replaceChild(div, pre);
 }
 
 var init = function () {
-    if ($("#func").length < 1) {
-        txtFormatter();
-        initFunctionSet();
-        console.log('load txt formatter success');
+    if ($('#func').length < 1) {
+        wrapAndFmt();
+        initFunc();
+        console.log('Load txt formatter success');
     }
 }();
